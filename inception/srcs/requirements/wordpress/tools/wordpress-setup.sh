e5595c3dfd05a318eec8bd9fa892b7299d08e402
Debian home dokers <grzegorz157@gmail.com>
2025-06-11 06:09:47 +0200

    nginx conf test

--- inception/srcs/requirements/wordpress/tools/wordpress-setup.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress-setup.sh
@@ -11,7 +11,7 @@ log() {
 
 log "Starting WordPress setup..."
 
-# --- Step 1: Verify that all required environment variables from .env are set ---
+# Verify that all required environment variables from .env are set ---
 REQUIRED_VARS=(
     "DOMAIN_NAME"
     "WP_ADMIN"
@@ -26,7 +26,7 @@ for var in "${REQUIRED_VARS[@]}"; do
 done
 # log "All required environment variables are present."
 
-# --- Step 2: Load secrets into environment variables for this script ---
+# Load secrets into environment variables for this script ---
 WORDPRESS_DB_NAME=$(cat /run/secrets/mysql_database)
 MYSQL_DB_USER=$(cat /run/secrets/mysql_user)
 MYSQL_DB_PASSWORD=$(cat /run/secrets/mysql_password)
@@ -37,35 +37,73 @@ WORDPRESS_DB_HOST="mariadb" # Explicitly add port for clarity
 
 log "Database credentials loaded from secrets."
 
-# --- Step 3: Check if WordPress is already configured ---
+# Check if WordPress is already configured ---
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     log "wp-config.php not found. Starting first-time installation..."
 
-   # --- Step 3a: Copy WordPress core only if not present ---
+   # Copy WordPress core only if not present ---
     if [ ! -f "/var/www/html/wp-load.php" ]; then
         log "Copying WordPress core to volume..."
         cp -R /tmp/wordpress/* /var/www/html/
         log "WordPress core copied."
     fi
 
-    cp wp-config-sample.php wp-config.php
-    
+    if ! command -v wp &> /dev/null; then
+        log "Downloading WP-CLI..."
+        curl -o wp-cli.phar https://github.com/wp-cli/wp-cli/releases/download/v2.10.0/wp-cli-2.10.0.phar
+        chmod +x wp-cli.phar
+        mv wp-cli.phar /usr/local/bin/wp
+        log "WP-CLI installed successfully."
+    else
+        log "wp-cli already installed"
+    fi
 
+    log "Waiting on database $WORDPRESS_DB_HOST..."
+    until wp db check --allow-root > /dev/null 2>&1; do
+        sleep 2
+    done
 
-    echo $MYSQL_DB_PASSWORD
+    # Creating wp-config.php
+    if [ ! -f wp-config.php ]; then
+        echo "Tworzę wp-config.php..."
+        wp config create \
+            --dbname="$WORDPRESS_DB_NAME" \
+            --dbuser="$MYSQL_DB_USER" \
+            --dbpass="$MYSQL_DB_PASSWORD" \
+            --dbhost="$WORDPRESS_DB_HOST" \
+            --skip-check \
+            --allow-root
 
-    sed -i \
-        -e "s/database_name_here/$WORDPRESS_DB_NAME/" \
-        -e "s/username_here/$MYSQL_DB_USER/" \
-        -e "s/password_here/$MYSQL_DB_PASSWORD/" \
-        -e "s/localhost/$WORDPRESS_DB_HOST/" \
-        wp-config.php
+        # Forcing domain and https
+        echo "define('WP_HOME', 'https://${DOMAIN_NAME}');" >> wp-config.php
+        echo "define('WP_SITEURL', 'https://${DOMAIN_NAME}');" >> wp-config.php
+    fi
 
-    # log "Downloading WP-CLI..."
-    # curl -o wp-cli.phar https://github.com/wp-cli/wp-cli/releases/download/v2.10.0/wp-cli-2.10.0.phar
-    # chmod +x wp-cli.phar
-    # mv wp-cli.phar /usr/local/bin/wp
-    # log "WP-CLI installed successfully."
+    # Install WordPress
+    if ! wp core is-installed --allow-root; then
+        echo "Instaluję WordPress..."
+        wp core install \
+            --url="https://${DOMAIN_NAME}" \
+            --title="${SITE_TITLE}" \
+            --admin_user="$WP_ADMIN" \
+            --admin_password="$WP_ADMIN_PASSWORD" \
+            --admin_email="$WP_ADMIN_EMAIL" \
+            --skip-email \
+            --allow-root
+
+        # Add regular user
+        wp user create "$WP_USER" "$WP_USER_EMAIL" \
+            --user_pass="$WP_USER_PASSWORD" \
+            --role=author \
+            --allow-root
+    fi
+else
+    log "wp-config.php already exists. Skipping installation."
+fi
+
+log "Starting PHP-FPM service..."
+# Execute the main container command (CMD from Dockerfile)
+exec "$@"
 
 
     # log "Creating admin user '${WP_ADMIN}'..."
@@ -110,10 +148,14 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
     # chown -R www-data:www-data /var/www/html
 
     # log "WordPress installation completed successfully!"
-else
-    log "wp-config.php already exists. Skipping installation."
-fi
 
-log "Starting PHP-FPM service..."
-# Execute the main container command (CMD from Dockerfile)
-exec "$@"
+
+    # log "Copying wp-config.php..."
+    # cp wp-config-sample.php wp-config.php
+    # log "Setting up wp-config.php..."
+    # sed -i \
+    #     -e "s/database_name_here/$WORDPRESS_DB_NAME/" \
+    #     -e "s/username_here/$MYSQL_DB_USER/" \
+    #     -e "s/password_here/$MYSQL_DB_PASSWORD/" \
+    #     -e "s/localhost/$WORDPRESS_DB_HOST/" \
+    #     wp-config.php
